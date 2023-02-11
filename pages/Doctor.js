4e6053e0db31606925c603113da534a5df046c20
Rohith710 <69068115+Rohith710@users.noreply.github.com>
2023-02-11 10:33:47 +0530

    Add files via upload

---
+++ pages/Doctor.js
@@ -0,0 +1,326 @@
+const Doctor = () => {
+  return (
+    <div className="relative bg-white w-full h-[1024px] overflow-hidden text-right text-base text-black font-be-vietnam-pro">
+      <iframe
+        className="[border:none] absolute top-[76px] left-[854px] w-[586px] h-[952px] overflow-auto"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d704627.7547976255!2d77.96256631738012!3d12.677144385392511!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1676090049892!5m2!1sen!2sin"
+      />
+      <div className="absolute top-[0px] left-[0px] bg-white shadow-[0px_2px_20px_rgba(0,_0,_0,_0.25)] w-[1440px] flex flex-row py-3 pr-[60px] pl-[22px] box-border items-start justify-start gap-[411px] text-left text-xl font-roboto">
+        <img
+          className="relative w-[176.41px] h-10 shrink-0 object-cover"
+          alt=""
+          src="../logo@2x.png"
+        />
+        <div className="rounded-[46px] bg-white shadow-[0px_4px_4px_rgba(0,_0,_0,_0.08)] box-border w-[300px] h-[47px] shrink-0 overflow-hidden flex flex-row py-2.5 pr-[7px] pl-[15px] items-center justify-between border-[1px] border-solid border-gainsboro-100">
+          <div className="relative font-medium">Headache</div>
+          <div className="rounded-[65px] bg-honeydew w-10 h-10 shrink-0 overflow-hidden flex flex-row p-2.5 box-border items-center justify-center">
+            <img
+              className="relative w-5 h-5 shrink-0 overflow-hidden"
+              alt=""
+              src="../search.svg"
+            />
+          </div>
+        </div>
+        <div className="rounded-[35px] box-border w-[101px] h-[47px] shrink-0 overflow-hidden flex flex-row py-[5px] pr-[17px] pl-2.5 items-center justify-start gap-[11px] border-[1px] border-solid border-darkgray">
+          <img
+            className="relative w-[30px] h-[30px] shrink-0 overflow-hidden"
+            alt=""
+            src="../menu.svg"
+          />
+          <img
+            className="relative w-[39.47px] h-[30px] shrink-0 object-cover"
+            alt=""
+            src="../image-14@2x.png"
+          />
+        </div>
+      </div>
+      <div className="absolute top-[85px] left-[26px] flex flex-row items-start justify-start gap-[4px] text-left text-lg font-roboto">
+        <div className="rounded-base bg-white flex flex-row py-[9px] px-[13px] items-center justify-center border-[1px] border-solid border-darkgray">
+          <div className="relative">Rating</div>
+        </div>
+        <div className="rounded-base bg-whitesmoke-200 flex flex-row py-[9px] px-[13px] items-center justify-start border-[1px] border-solid border-black">
+          <div className="relative">Availability</div>
+        </div>
+        <div className="rounded-base bg-white flex flex-row py-[9px] px-[13px] items-center justify-center border-[1px] border-solid border-darkgray">
+          <div className="relative">Distance</div>
+        </div>
+        <div className="rounded-base bg-white flex flex-row py-[9px] px-[13px] items-center justify-center border-[1px] border-solid border-darkgray">
+          <div className="relative">Speciality</div>
+        </div>
+        <div className="rounded-base bg-whitesmoke-200 flex flex-row py-[9px] px-[13px] items-center justify-start border-[1px] border-solid border-black">
+          <div className="relative">More filters . 1</div>
+        </div>
+      </div>
+      <div className="absolute top-[132px] left-[25px] rounded-base bg-white shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[813px] h-[265px] overflow-hidden">
+        <b className="absolute top-[115px] left-[8px]">Patients</b>
+        <div className="absolute top-[39px] left-[150px] flex flex-row items-end justify-start gap-[40px]">
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+                src="../ellipse-98.svg"
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+                src="../ellipse-981.svg"
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+                src="../ellipse-981.svg"
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[414px] left-[22px] rounded-base bg-white shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[816px] h-[265px] overflow-hidden">
+        <b className="absolute top-[124px] left-[8px]">My Patients</b>
+        <div className="absolute top-[39px] left-[152px] flex flex-row items-end justify-start gap-[40px]">
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+                src="../ellipse-98.svg"
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+                src="../ellipse-981.svg"
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+                src="../ellipse-981.svg"
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+                src="../ellipse-986.svg"
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[703px] left-[25px] rounded-base bg-white shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[816px] h-[265px] overflow-hidden">
+        <div className="absolute top-[39px] left-[152px] flex flex-row items-end justify-start gap-[40px]">
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+                src="../ellipse-986.svg"
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+                src="../rectangle-242.svg"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-start gap-[30px]">
+            <div className="relative w-[140px] h-[140px] shrink-0">
+              <div className="absolute top-[0px] left-[0px] rounded-base bg-gainsboro-300 w-[140px] h-[140px]" />
+              <img
+                className="absolute top-[6px] left-[123px] w-2.5 h-2.5"
+                alt=""
+              />
+            </div>
+            <div className="flex flex-row items-start justify-start gap-[25px]">
+              <div className="relative rounded-base bg-mediumblue w-10 h-[18px] shrink-0" />
+              <img
+                className="relative rounded-base w-10 h-[18px] shrink-0"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <b className="absolute top-[119px] left-[12px]">Facility</b>
+      </div>
+      <img
+        className="absolute top-[409px] left-[1399px] w-[41px] h-[103px] overflow-hidden opacity-[0.5]"
+        alt=""
+        src="../frame-290.svg"
+      />
+    </div>
+  );
+};
+
+export default Doctor;
